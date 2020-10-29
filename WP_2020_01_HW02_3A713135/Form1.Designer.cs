5e59c3fba7c52572cbafe51bcc2bab33f59fc9e0
mille22554 <mille22554@gmail.com>
2020-10-29 20:24:08 +0800

    雛形1.2

--- WP_2020_01_HW02_3A713135/Form1.Designer.cs
+++ WP_2020_01_HW02_3A713135/Form1.Designer.cs
@@ -53,6 +53,7 @@
             this.button1.TabIndex = 1;
             this.button1.Text = "抽卡";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
--- WP_2020_01_HW02_3A713135/Form1.cs
+++ WP_2020_01_HW02_3A713135/Form1.cs
@@ -12,6 +12,8 @@ namespace WP_2020_01_HW02_3A713135
 {
     public partial class Form1 : Form
     {
+        int x = 0;
+        string a;
         public Form1()
         {
             InitializeComponent();
@@ -19,10 +21,16 @@ namespace WP_2020_01_HW02_3A713135
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            string a= "抽過的卡:\n";
-            label1.Text = a;
+            label1.Text = "抽過的卡:";
             label2.Text = "作者:Moro";
             this.Text = "抽卡";
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            x++;
+            a = a + x + ":\r\n";
+            textBox1.Text = a;
         }
     }
 }
